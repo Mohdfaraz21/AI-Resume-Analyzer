a40489a78be750676fe95041b1c4251bb4b88f5c
Mohdfaraz21 <mofaraz7800@gmail.com>
2026-06-16 13:28:49 +0530

    fetch real resume to the home page

--- app/components/ResumeCard.tsx
+++ app/components/ResumeCard.tsx
@@ -1,12 +1,24 @@
-import { Link } from "react-router";
+import {Link} from "react-router";
 import ScoreCircle from "./ScoreCircle";
-import { useState } from "react";
+import {useEffect, useState} from "react";
+import {usePuterStore} from "../lib/puter";
 
 const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath } }: { resume: Resume }) => {
-
+    const { fs } = usePuterStore();
     const [resumeUrl, setResumeUrl] = useState('');
-    
-   return (
+
+    useEffect(() => {
+        const loadResume = async () => {
+            const blob = await fs.read(imagePath);
+            if(!blob) return;
+            let url = URL.createObjectURL(blob);
+            setResumeUrl(url);
+        }
+
+        loadResume();
+    }, [imagePath]);
+
+    return (
         <Link to={`/resume/${id}`} className="resume-card animate-in fade-in duration-1000">
             <div className="resume-card-header">
                 <div className="flex flex-col gap-2">
@@ -30,7 +42,6 @@ const ResumeCard = ({ resume: { id, companyName, jobTitle, feedback, imagePath }
                 </div>
                 )}
         </Link>
-   )
-};
-
-export default ResumeCard;
+    )
+}
+export default ResumeCard
